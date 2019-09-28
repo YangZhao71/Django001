9ae25f4b7b707b18e84fe89c5ef411adb49dd6c4
YangZ <zymy701@gmail.com>
2019-09-28 19:30:45 -0400

    CURD done

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+	title = models.TextField(blank=True, null=True)
+	# image = models.ImageField 
+	# not powerful enough, install an external library
+	image = ProcessedImageField(
+			upload_to = 'static/images/posts',
+			format = 'JPEG',
+			options = {'quality': 100},
+			blank = True,
+			null = True
+		) 
+
+	def get_absolute_url(self):
+		return reverse("post_detail", args=[str(self.id)])
+
